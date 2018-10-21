9c00aff4da991d5d916b761fa1b01b0ba74e222c
Phan Trinh Ha <mac@MACs-MacBook-Air.local>
2018-10-21 16:42:15 -0400

    - Add requirements.txt - Remake settings.py

--- recommender/recommender/settings.py
+++ recommender/recommender/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-import secret
+from .secret import secret_key
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
