62fcb23d8d4573793883f9cb5eb66b88bf0b3a03
Phan Trinh Ha <mac@MACs-MacBook-Air.local>
2018-11-19 20:02:14 -0500

    Available to get access tokens

--- recommender/mainapp/urls.py
+++ recommender/mainapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('callback/q', views.callback, name = 'callback')
 ]
--- recommender/mainapp/views.py
+++ recommender/mainapp/views.py
@@ -1,6 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from recommender.secret import client_ID, client_secret
 import requests
+import json
+import urllib
+import base64
+
 # Create your views here.
+PORT = 8000
+
+# URL end-point
+AUTH_URL = "https://accounts.spotify.com/authorize"
+API_URL = 'https://api.spotify.com/v1/'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+CALLBACK_URL = 'callback/q'
+redirect_uri = 'http://127.0.0.1:' + '{}/{}'.format(PORT, CALLBACK_URL)
+
+# scopes
+scopes = [
+    'user-read-private',
+]
+
+args_list = {
+    'response_type' : 'code',
+    'client_id' : client_ID,
+    'scope' : ' '.join(scopes),
+    'redirect_uri' : redirect_uri,
+}
+
+# Auth token
+auth_token = None
+
 
 def index(request):
-    return render(request, 'mainapp/index.html', {})
+    url_args = '&'.join(['{}={}'.format(key, val)
+                        for key, val in args_list.items()])
+    auth_path = '{}?{}'.format(AUTH_URL, url_args)
+    return redirect(auth_path)
+
+
+def callback(request):
+    code = request.GET.get('code')
+
+    data = {
+        'grant_type' : 'authorization_code',
+        'code' : code,
+        'redirect_uri' : redirect_uri
+    }
+
+    encoded_client_info = base64.standard_b64encode(
+                            '{}:{}'.format(client_ID, client_secret).encode())
+
+    header = {
+        'Authorization' : 'Basic {}'.format(encoded_client_info.decode())
+    }
+
+    post_request = requests.post(TOKEN_URL, data=data, headers=header)
+
+    response_data = json.loads(post_request.text)
+
+    result = {'result' : json.dumps(response_data)}
+
+    return render(request, 'mainapp/index.html', result)
